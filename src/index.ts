2a3693638c6f78f13ea4b8b5d82f39bdbf3614b6
chimame <rito.tamata@gmail.com>
2023-10-01 19:36:12 +0900

    Add check read performance function

--- src/index.ts
+++ src/index.ts
@@ -50,8 +50,10 @@ app.get("/write", async (c) => {
     const start = performance.now()
     const db = connection(c.req.query('type') === 'pg' ? c.env.DATABASE_URL : c.env.DB)
 
-    const now = new Date().getTime()
-    await db.insertInto('Users').values({ username: `test${now}`, email: `test${now}@example${now}.com` }).execute()
+    const min = 200000
+    const max = 999999
+    const id = Math.floor(Math.random() * (max - min + 1)) + min
+    await db.insertInto('Users').values({ id, username: `test${id}`, email: `test${id}@example${id}.com` }).execute()
 
     return c.text(`write ${performance.now() - start}ms`)
   } catch (e) {
@@ -67,6 +69,27 @@ app.get("/read", async (c) => {
     const data = await db.selectFrom('Users').selectAll().orderBy('Users.id desc').limit(50).execute()
 
     return c.json({
+      type: 'read 1',
+      time: performance.now() - start,
+      data
+    })
+  } catch (e) {
+    return c.text((e as Error).message)
+  }
+})
+
+app.get("/read/100", async (c) => {
+  try {
+    const start = performance.now()
+    const db = connection(c.req.query('type') === 'pg' ? c.env.DATABASE_URL : c.env.DB)
+
+    for(let i = 0; i < 999; i++) {
+      await db.selectFrom('Users').selectAll().orderBy('Users.id desc').limit(50).execute()
+    }
+    const data = await db.selectFrom('Users').selectAll().orderBy('Users.id desc').limit(50).execute()
+
+    return c.json({
+      type: 'read 100',
       time: performance.now() - start,
       data
     })
@@ -83,6 +106,27 @@ app.get("/read/:name", async (c) => {
     const data = await db.selectFrom('Users').selectAll().where('Users.email', 'like', `%${c.req.param('name')}%`).execute()
 
     return c.json({
+      type: `read name: ${c.req.param('name')} 1`,
+      time: performance.now() - start,
+      data
+    })
+  } catch (e) {
+    return c.text((e as Error).message)
+  }
+})
+
+app.get("/read/:name/100", async (c) => {
+  try {
+    const start = performance.now()
+    const db = connection(c.req.query('type') === 'pg' ? c.env.DATABASE_URL : c.env.DB)
+
+    for(let i = 0; i < 999; i++) {
+      await db.selectFrom('Users').selectAll().where('Users.email', 'like', `%${c.req.param('name')}%`).execute()
+    }
+    const data = await db.selectFrom('Users').selectAll().where('Users.email', 'like', `%${c.req.param('name')}%`).execute()
+
+    return c.json({
+      type: `read name: ${c.req.param('name')} 100`,
       time: performance.now() - start,
       data
     })
